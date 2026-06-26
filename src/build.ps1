1fd73f888ca7b95cbd2b4e935afdd19ad8fe85e4
Bastian Eicher <bastian@eicher.net>
2026-06-26 12:19:03 +0200

    Tweaked build script

--- src/build.ps1
+++ src/build.ps1
@@ -18,7 +18,7 @@ function Run-MSBuild {
 
 # Build
 if ($env:CI) { $ci = "/p:ContinuousIntegrationBuild=True" }
-Run-MSBuild /v:Quiet /t:Restore /t:Build $ci /p:Configuration=Release /p:Version=$Version ZeroInstall.Publish.Cli.slnx
+Run-MSBuild /v:Quiet /Restore /t:Build $ci /p:Configuration=Release /p:Version=$Version ZeroInstall.Publish.Cli.slnx
 Remove-Item ..\artifacts\Release\net48 -Recurse -Include *.xml,*.pdb,Microsoft.CodeAnalysis*
 
 popd
